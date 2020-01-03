2a6c1368df049b4adabd54c34b5e4cc9c22da552
saskaio <saskafon@gmail.com>
2020-01-03 21:41:01 +0100

    Created Scores in Ranking list

--- js/script.js
+++ js/script.js
@@ -41,3 +41,42 @@ function deleteUser(username) {
 function editUser(id) {
   window.location.href = "edit-profile.php?id="+id;
 }
+
+// /*Sort ranking list ASC*/
+// function sortASC() {
+//   // console.log("http://localhost/sudoku/back-end/users?user_name="+user_name+"&name="+name+"&lastname="+lastname);
+//   $.ajax({
+//     url: "http://localhost/sudoku/back-end/results",
+//     type: 'GET',
+//     success: function (result) {
+//       result.forEach((element, i) => {
+//    //sve moje lijeve iz phpa
+//           $( ".rank ul" ).append( "<li class='row'><em>" + (i+1) + "</em><em>"+ element.user_name +"</em><em>"+ element.name +"</em><em>"+ element.lastname +"</em><em>"+ element.score +"</em><em class='icons'>"+<i class="fas fa-sort-up" onclick="sortASC(1)"></i>+"</em></li>" );
+       
+//       });
+//     },
+//     error: function (result) {
+//       console.log(result);
+//     }
+//   });
+// }
+
+
+// /*Sort ranking list DESC*/
+// function sortDESC() {
+//     // console.log("http://localhost/sudoku/back-end/users?user_name="+user_name+"&name="+name+"&lastname="+lastname);
+//     $.ajax({
+//       url: "http://localhost/sudoku/back-end/results",
+//       type: 'GET',
+//       success: function (result) {
+//         result.forEach((element, i) => {
+//      //sve moje lijeve iz phpa
+//             $( ".rank ul" ).append( "<li class='row'><em>" + (i+1) + "</em><em>"+ element.user_name +"</em><em>"+ element.name +"</em><em>"+ element.lastname +"</em><em>"+ element.score +"</em><em class='icons'>"+<i class="fas fa-sort-down" onclick="sortDESC(0)"></i>+"</em></li>" );
+         
+//         });
+//       },
+//       error: function (result) {
+//         console.log(result);
+//       }
+//     });
+//   }
--- rank.php
+++ rank.php
@@ -1,4 +1,3 @@
-
 <?php
 include("back-end/user.php");
 session_start();
@@ -6,52 +5,74 @@ if (!isset($_SESSION['user'])) {
     header("Location: login.php");
 }
 
- $ch = curl_init('http://localhost/sudoku/back-end/users');
- curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
- $result = curl_exec($ch);
- $result_json = json_decode($result);
+$ch = curl_init('http://localhost/sudoku/back-end/results');
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$result = curl_exec($ch);
+$result_json = json_decode($result);
+//  var_dump($result_json);
+//  die();
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <link rel="stylesheet" type="text/css" href="css/reset.css">
-	<link rel="stylesheet" type="text/css" href="css/main-style.css">
-	<link rel="stylesheet" type="text/css" href="css/style-responsive.css">
+    <link rel="stylesheet" type="text/css" href="css/main-style.css">
+    <link rel="stylesheet" type="text/css" href="css/style-responsive.css">
     <link rel="stylesheet" type="text/css" href="fonts/font-style.css">
+    <script src="https://code.jquery.com/jquery-3.4.1.js" integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU=" crossorigin="anonymous"></script>
+    <script src="js/script.js"></script>
+    <script type="text/javascript">
+        // $(document).ready(function(){
+        //     sort();
+        // });
+        (function() {
+            var css = document.createElement('link');
+            css.href = 'https://use.fontawesome.com/releases/v5.1.0/css/all.css';
+            css.rel = 'stylesheet';
+            css.type = 'text/css';
+            document.getElementsByTagName('head')[0].appendChild(css);
+        })();
+    </script>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>All users</title>
 </head>
+
 <body>
     <main>
         <section class="welcome">
-        <div class="rank">
-            <h5>Ranking list</h5>
-            <ul>
-                <li>
-                    <em>&#8470;</em>
-                    <em>Username</em>
-                    <em>Name</em>
-                    <em>Lastname</em>
-                    <em>Score</em>
-                </li>
-                <?php
-                foreach($result_json as $key => $user){?>
-                <li class="row">
-                    <em><?php echo $key+1; ?></em>
-                    <em><?php echo $user->user_name; ?></em>
-                    <em><?php echo $user->name; ?></em>
-                    <em><?php echo $user->lastname; ?></em>
-                    <em><?php //echo $user->score;?></em>
-                </li>
-                <?php } ?>
-            </ul>
-        </div>
+            <div class="rank">
+                <h5>Ranking list</h5>
+                <ul>
+                    <li class="column">
+                        <em>&#8470;</em>
+                        <em>Username</em>
+                        <em>Name</em>
+                        <em>Lastname</em>
+                        <em>Score</em>
+                        <em class="icons">
+                            <i class="fas fa-sort-up"></i></i>
+                            <i class="fas fa-sort-down"></i>
+                        </em>
+                    </li>
+                    <?php
+                    foreach ($result_json as $key => $user) { ?>
+                        <li class="row">
+                            <em><?php echo $key + 1; ?></em>
+                            <em><?php echo $user->user_name; ?></em>
+                            <em><?php echo $user->name; ?></em>
+                            <em><?php echo $user->lastname; ?></em>
+                            <em><?php echo $user->timeInSeconds; ?></em>
+                        </li>
+                    <?php }?>
+                </ul>
+            </div>
         </section>
     </main>
-    
+
 </body>
-<script src="https://code.jquery.com/jquery-3.4.1.js" integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU=" crossorigin="anonymous"></script>
-<script src="js/script.js"></script>
+
+
 </html>
