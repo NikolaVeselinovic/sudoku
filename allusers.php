9b0dec230c4eeaba4d22fe3cb16f5e730c74b746
NikolaV <nikolaveselinovic388@gmail.com>
2020-01-03 21:40:47 +0100

    Create search input on allusers page

--- allusers.php
+++ allusers.php
@@ -1,4 +1,3 @@
-
 <?php
 include("back-end/user.php");
 session_start();
@@ -6,59 +5,86 @@ if (!isset($_SESSION['user'])) {
     header("Location: login.php");
 }
 
- $ch = curl_init('http://localhost/sudoku/back-end/users');
- curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
- $result = curl_exec($ch);
- $result_json = json_decode($result);
+$ch = curl_init('http://localhost/sudoku/back-end/users');
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$result = curl_exec($ch);
+$result_json = json_decode($result);
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <link rel="stylesheet" type="text/css" href="css/reset.css">
-	<link rel="stylesheet" type="text/css" href="css/main-style.css">
-	<link rel="stylesheet" type="text/css" href="css/style-responsive.css">
+    <link rel="stylesheet" type="text/css" href="css/main-style.css">
+    <link rel="stylesheet" type="text/css" href="css/style-responsive.css">
     <link rel="stylesheet" type="text/css" href="fonts/font-style.css">
-    <script type="text/javascript"> (function() { var css = document.createElement('link'); css.href = 'https://use.fontawesome.com/releases/v5.1.0/css/all.css'; css.rel = 'stylesheet'; css.type = 'text/css'; document.getElementsByTagName('head')[0].appendChild(css); })(); </script>
+    <script src="https://code.jquery.com/jquery-3.4.1.js" integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU=" crossorigin="anonymous"></script>
+    <script src="js/script.js"></script>
+    <script type="text/javascript">
+    
+         $(document).ready(function(){
+            getAllUsers('<?php echo $_SESSION['user']->id;?>', '', '', '');
+        });
+
+        (function() {
+            var css = document.createElement('link');
+            css.href = 'https://use.fontawesome.com/releases/v5.1.0/css/all.css';
+            css.rel = 'stylesheet';
+            css.type = 'text/css';
+            document.getElementsByTagName('head')[0].appendChild(css);
+        })();
+    </script>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>All users</title>
 </head>
+
 <body>
     <main>
         <section class="welcome">
-        <div class="allusers">
-            <h5>All users</h5>
-            <ul>
-                <li>
-                    <em>&#8470;</em>
-                    <em>Username</em>
-                    <em>Name</em>
-                    <em>Lastname</em>
-                    <em></em>
-                </li>
-                <?php
-                foreach($result_json as $key => $user){?>
-                <li class="row">
-                    
-                    <em><?php echo $key+1; ?></em>
-                    <em><?php echo $user->user_name; ?></em>
-                    <em><?php echo $user->name; ?></em>
-                    <em><?php echo $user->lastname; ?></em>
-                    <em class="icons">
-                        <?php if($user->user_name != $_SESSION['user']->user_name){?>
-                        <i class="fas fa-trash-alt" onclick="deleteUser('<?php echo $user->user_name; ?>')"></i>
-                        <?php } ?>
-                        <i class="fas fa-pencil-alt" onclick="editUser('<?php echo $user->id; ?>')"></i>
-                    </em>
-                </li>
-                <?php } ?>
-            </ul>
-        </div>
+            <div class="allusers">
+                <h5>All users</h5>
+                <ul>
+                    <li>
+                        <em>&#8470;</em>
+                        <em>Username</em>
+                        <em>Name</em>
+                        <em>Lastname</em>
+                        <em></em>
+                    </li>
+                    <li class="search">
+                        <form autocomplete="off" action="" name="search" class="form">
+                            <em></em>
+                            <em><input type="text" name="username" placeholder="Username"></em>
+                            <em><input type="text" name="name" placeholder="Name"></em>
+                            <em> <input type="text" name="lastname" placeholder="Lastname"></em>
+                            <em></em>
+                        </form>
+                    </li>
+
+                    <?php
+                    /* foreach ($result_json as $key => $user) { ?>
+                        <li class="row">
+
+                            <em><?php echo $key + 1; ?></em>
+                            <em><?php echo $user->user_name; ?></em>
+                            <em><?php echo $user->name; ?></em>
+                            <em><?php echo $user->lastname; ?></em>
+                            <em class="icons">
+                                <?php if ($user->user_name != $_SESSION['user']->user_name) { ?>
+                                    <i class="fas fa-trash-alt" onclick="deleteUser('<?php echo $user->user_name; ?>')"></i>
+                                <?php } ?>
+                                <i class="fas fa-pencil-alt" onclick="editUser('<?php echo $user->id; ?>')"></i>
+                            </em>
+                        </li>
+                                <?php }*/ ?>
+                </ul>
+            </div>
         </section>
     </main>
-    
+
 </body>
-<script src="https://code.jquery.com/jquery-3.4.1.js" integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU=" crossorigin="anonymous"></script>
-<script src="js/script.js"></script>
+
+
 </html>
--- js/script.js
+++ js/script.js
@@ -1,43 +1,78 @@
 var icons = false;
 
-$( ".row" ).hover( function(){
-  if (icons == false) {
-    $(this).children('.icons').css('visibility', 'visible');
-  } else {
-    $('.icons').css('visibility', 'hidden');
-  }
-  icons=!icons;
-}
-);
+// $(".row").hover(function () {
+//   console.log("asd");
+//   if (icons == false) {
+//     $(this).children('.icons').css('visibility', 'visible');
+//   } else {
+//     $('.icons').css('visibility', 'hidden');
+//   }
+//   icons = !icons;
+// }
+// );
+
+// $(".row").mouseenter(function () {
+//   console.log("asd");
+//   if (icons == false) {
+//     $(this).children('.icons').css('visibility', 'visible');
+//   } else {
+//     $('.icons').css('visibility', 'hidden');
+//   }
+//   icons = !icons;
+// });
 function logout() {
   console.log("izlogovan");
   $.get("logout.php", function () {
-      window.location = "login.php";
-    });
+    window.location = "login.php";
+  });
 }
-
-
-
-
-
-
-
 
 /*Delete user*/
 function deleteUser(username) {
-    $.ajax({
-      url: "http://localhost/sudoku/back-end/user/"+username,
-      type: 'DELETE',
-      success: function(result) {
-          console.log(result);
-      },
-      error:  function(result) {
-        console.log(result);
+  $.ajax({
+    url: "http://localhost/sudoku/back-end/user/" + username,
+    type: 'DELETE',
+    success: function (result) {
+      console.log(result);
+    },
+    error: function (result) {
+      console.log(result);
     }
   });
 }
 
 /*Edit user */
 function editUser(id) {
-  window.location.href = "edit-profile.php?id="+id;
+  window.location.href = "edit-profile.php?id=" + id;
+}
+
+function getAllUsers(id) {
+  $.ajax({
+    url: "http://localhost/sudoku/back-end/users",
+    type: 'GET',
+    success: function (result) {
+      result.forEach((element, i) => {
+        if(element.id == id){
+          $( ".allusers ul" ).append( "<li class='row'><em>" + (i+1) + "</em><em>"+ element.user_name +"</em><em>"+ element.name +"</em><em>"+ element.lastname 
+          +"</em><em class='icons'><i class='fas fa-pencil-alt' onclick='editUser(\"" + element.id+ "\")'></i></em></li>" );
+        }else{
+          $( ".allusers ul" ).append( "<li class='row'><em>" + (i+1) + "</em><em>"+ element.user_name +"</em><em>"+ element.name +"</em><em>"+ element.lastname 
+           +"</em><em class='icons'><i class='fas fa-trash-alt' onclick='deleteUser(\"" + element.user_name + 
+            "\")'></i>&nbsp;<i class='fas fa-pencil-alt' onclick='editUser(\"" + element.id+ "\")'></i></em></li>" );
+        }
+      });
+      $(".row").hover(function () {
+        if (icons == false) {
+          $(this).children('.icons').css('visibility', 'visible');
+        } else {
+          $('.icons').css('visibility', 'hidden');
+        }
+        icons = !icons;
+      }
+      );
+    },
+    error: function (result) {
+      console.log(result);
+    }
+  });
 }
